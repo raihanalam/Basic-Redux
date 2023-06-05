d39785ed9e6f7789ba7ebf5a9990c093f4911261
raihanalam <raihanhridoybd@gmail.com>
2023-06-05 10:36:48 -0700

    Redux Basic

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+const redux = require('redux')
+
+//State
 const initState = {
     num: 0
 }
 
 //Reducer
 const rootReducer = (state = initState, action)=>{
+    if (action.type === 'INC_NUM'){
+        return{
+            ...state,
+            num: state.num + 1
+        }
+    }
+
+    if (action.type === 'ADD_NUM'){
+        return{
+            ...state,
+            num: state.num + action.value
+        }
+    }
     return state;
 }
 
 //Store
 const store = redux.createStore(rootReducer);
+
+console.log(store.getState());
+
+
+//Dispatching Action
+store.dispatch({
+    type: 'INC_NUM'
+})
+
+store.dispatch({
+    type: 'ADD_NUM',
+    value: 35
+})
 
 console.log(store.getState());
