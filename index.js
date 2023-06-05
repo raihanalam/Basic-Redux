c54138ab6046c8c46bda5276cb013f027a85b5cf
raihanalam <raihanhridoybd@gmail.com>
2023-06-05 10:21:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const initState = {
+    num: 0
+}
+
+//Reducer
+const rootReducer = (state = initState, action)=>{
+    return state;
+}
+
+//Store
+const store = redux.createStore(rootReducer);
+
+console.log(store.getState());
